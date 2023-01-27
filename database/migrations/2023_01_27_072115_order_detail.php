43668ee6e08ee01b62648318970b70d53b46fc8b
DongT2LS <vanhein445566@gmail.com>
2023-01-27 15:06:35 +0700

    update model 27/1

---
+++ database/migrations/2023_01_27_072115_order_detail.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OrderDetail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->unsignedBigInteger('bill_id');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('bill_id')->references('id')->on('bills');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_detail');
+    }
+}
